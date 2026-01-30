79004ea8cd5fc4276fc0b82d1f822a51d05be820
Kadu Fuziki <59468628+duki2k@users.noreply.github.com>
2026-01-30 17:32:07 -0300

    Update news.py

--- news.py
+++ news.py
@@ -6,7 +6,7 @@ def noticias():
         "category": "business",
         "language": "pt",
         "pageSize": 5,
-        "apiKey": "SUA_API_KEY_NEWSAPI"
+        "apiKey": "939f315d880e41d7983dedb3ab0c98f1"
     }
 
     r = requests.get(url, params=params).json()
