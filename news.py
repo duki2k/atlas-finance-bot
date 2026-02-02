b82253f628870e895c6334fea32663da9b37c4cb
Kadu Fuziki <59468628+duki2k@users.noreply.github.com>
2026-02-01 21:06:41 -0300

    Update news.py

--- news.py
+++ news.py
@@ -2,23 +2,13 @@ import feedparser
 
 RSS_URL = (
     "https://news.google.com/rss/search"
-    "?q=bolsas+mundiais+mercado+financeiro+wall+street+economia"
+    "?q=mercado+financeiro+bolsa+wall+street+cripto"
     "&hl=pt-BR&gl=BR&ceid=BR:pt-419"
 )
 
 def noticias():
     try:
         feed = feedparser.parse(RSS_URL)
-
-        if not feed.entries:
-            return []
-
-        titulos = []
-        for entry in feed.entries[:8]:
-            titulos.append(entry.title.strip())
-
-        return titulos
-
-    except Exception as e:
-        print("Erro ao buscar notícias:", e)
+        return [entry.title for entry in feed.entries[:8]]
+    except:
         return []
