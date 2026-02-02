d78cb378092ede6c931d7c0441fd455be4696d3e
Kadu Fuziki <59468628+duki2k@users.noreply.github.com>
2026-02-01 21:50:32 -0300

    Update telegram.py

--- telegram.py
+++ telegram.py
@@ -6,11 +6,9 @@ TELEGRAM_CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
 
 def enviar_telegram(texto):
     if not TELEGRAM_TOKEN or not TELEGRAM_CHAT_ID:
-        print("Telegram não configurado")
         return False
 
     url = f"https://api.telegram.org/bot{TELEGRAM_TOKEN}/sendMessage"
-
     payload = {
         "chat_id": TELEGRAM_CHAT_ID,
         "text": texto
