6b3620e2a8b4a1a7bcf6f8fd76e0a9f4ad4f3833
Kadu Fuziki <59468628+duki2k@users.noreply.github.com>
2026-02-01 21:50:07 -0300

    Update news.py

--- news.py
+++ news.py
@@ -2,13 +2,13 @@ import feedparser
 
 RSS_URL = (
     "https://news.google.com/rss/search"
-    "?q=mercado+financeiro+bolsa+wall+street+cripto"
+    "?q=mercado+financeiro+bolsa+cripto+economia"
     "&hl=pt-BR&gl=BR&ceid=BR:pt-419"
 )
 
 def noticias():
     try:
         feed = feedparser.parse(RSS_URL)
-        return [entry.title for entry in feed.entries[:8]]
+        return [e.title for e in feed.entries[:8]]
     except:
         return []
