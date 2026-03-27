849541df32f1c75035558a0ff0dc24c7c536c210
Kadu Fuziki <59468628+duki2k@users.noreply.github.com>
2026-03-27 05:59:43 -0300

    Update config.py

--- scr/bot/config.py
+++ scr/bot/config.py
@@ -3,18 +3,24 @@ from dotenv import load_dotenv
 load_dotenv()
 
 DISCORD_TOKEN = os.getenv('DISCORD_TOKEN')
+GUILD_ID = 1486268008266596443  # Seu server
 
-# 👇 SEUS IDs AQUI 👇
 CHANNELS = {
-    'welcome_logs': 1486268009550188556,  # #welcome-logs
-    'admin_cmds': 1486639291739144212,    # #admin-cmds
-    'add_farms': 1486642954398466088,     # #add-farms (admin only)
-    'pvp_events': 1486645052653437059,    # #pvp-events
-    'leaderboard': 1486268011823366218
+    'admin_cmds': 1486639291739144212,      # #comandos-admin
+    'farm_add': 1486645052653437059,        # #acao-add  
+    'events_farm': 1486642954398466088,     # #evento-farm
+    'events_pvp': 1486268011823366216,      # #evento-acao
+    'leaderboard': 1486268011823366218,     # #rank-semanal
+    'welcome_logs': 1486268009550188556     # #entradas-saida
 }
 
 ROLES = {
-    'admin': [1486268008409206867, 1486268008409206866, 1486268008409206864],
-    'pvp_member': 1486268008266596448,   # Para participar PVP
-    'farm_king': 1486268008409206864
+    'farm_king': 1487011323484045462,           # Oposição da semana
+    'pvp_members': [1486268008409206866],       # PVP cargos
+    'admins': [
+        1486268008409206867,  # Cúpula
+        1486268008409206864,
+        1486268008266596449,
+        1486268008266596448
+    ]
 }
